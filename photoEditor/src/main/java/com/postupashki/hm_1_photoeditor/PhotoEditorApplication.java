103ee535ad4c57b13a64ca948f695579f69f5dcc
alexanderbibik <sbibik2000@mail.ru>
2024-08-16 09:48:03 +0300

    feat: инициализация проекта

---
+++ photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/PhotoEditorApplication.java
@@ -0,0 +1,13 @@
+package com.postupashki.hm_1_photoeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoEditorApplication.class, args);
+    }
+
+}
---
+++ photoEditor/src/test/java/com/postupashki/hm_1_photoeditor/PhotoEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postupashki.hm_1_photoeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
