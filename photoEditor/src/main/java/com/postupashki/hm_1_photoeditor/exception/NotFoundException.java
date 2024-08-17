93e472eb6766e11a3f4e79f4b60cebe9f5bfdf4f
alexanderbibik <sbibik2000@mail.ru>
2024-08-17 15:17:58 +0300

    feat: добавлена кастомная ошибка при валидации случая, когда в хранилище нет нужной таски

---
+++ photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.postupashki.hm_1_photoeditor.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
