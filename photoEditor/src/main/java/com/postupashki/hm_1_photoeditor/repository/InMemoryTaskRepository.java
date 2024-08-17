bd334729ea4dabdab91a359f18fc5a57c5b14e4e
alexanderbibik <sbibik2000@mail.ru>
2024-08-17 15:20:01 +0300

    fix: реализация имплементированных методов in memory хранилища

--- photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/repository/InMemoryTaskRepository.java
+++ photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/repository/InMemoryTaskRepository.java
@@ -1,13 +1,13 @@
 package com.postupashki.hm_1_photoeditor.repository;
 
 import com.postupashki.hm_1_photoeditor.entity.Task;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface InMemoryTaskRepository {
     Task getTaskById(UUID taskId);
 
-    Task getTaskStatusById(UUID taskId);
+    Task createTask();
+
+    void save(Task task);
 }
--- photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/repository/impl/InMemoryTaskRepositoryImpl.java
+++ photoEditor/src/main/java/com/postupashki/hm_1_photoeditor/repository/impl/InMemoryTaskRepositoryImpl.java
@@ -1,18 +1,36 @@
 package com.postupashki.hm_1_photoeditor.repository.impl;
 
 import com.postupashki.hm_1_photoeditor.entity.Task;
+import com.postupashki.hm_1_photoeditor.entity.TaskStatusEnum;
 import com.postupashki.hm_1_photoeditor.repository.InMemoryTaskRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class InMemoryTaskRepositoryImpl implements InMemoryTaskRepository {
+
+    private static Map<UUID, Task> database = new ConcurrentHashMap<>();
+
     @Override
     public Task getTaskById(UUID taskId) {
-        return null;
+        return database.get(taskId);
     }
 
     @Override
-    public Task getTaskStatusById(UUID taskId) {
-        return null;
+    public Task createTask() {
+        Task newTask = new Task();
+        newTask.setId(UUID.randomUUID());
+        newTask.setTaskStatus(TaskStatusEnum.IN_PROGRESS);
+
+        save(newTask);
+        return newTask;
+    }
+
+    @Override
+    public void save(Task task) {
+        database.put(task.getId(), task);
     }
 }
